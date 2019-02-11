adee766f9723a0ebfa10553c80151de2039737e3
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-11 18:26:02 -0500

    WIP for CachedURLTitle.title

--- urltitle/urltitle.py
+++ urltitle/urltitle.py
@@ -1,7 +1,9 @@
 import logging
+import time
+
+import requests
 
 from cachetools.func import ttl_cache
-import requests
 
 from . import config
 
@@ -10,11 +12,21 @@ log = logging.getLogger(__name__)
 
 class CachedURLTitle:
     def __init__(self,
-                 max_cache_size: int = config.DEFAULT_MAX_CACHE_SIZE,cache_ttl: float = config.DEFAULT_CACHE_TTL):
-        log.debug('Max cache size is %s and cache TTL is %s seconds.', max_cache_size, cache_ttl)
-        self.title = ttl_cache(maxsize=max_cache_size, ttl=cache_ttl)(self.title)   # type: ignore  # Instance level cache
+                 cache_max_size: int = config.DEFAULT_CACHE_MAX_SIZE, cache_ttl: float = config.DEFAULT_CACHE_TTL):
+        log.debug('Max cache size is %s and cache TTL is %s seconds.', cache_max_size, cache_ttl)
+        self.title = ttl_cache(maxsize=cache_max_size, ttl=cache_ttl)(self.title)   # type: ignore  # Instance level cache
 
     def title(self, url: str):
-        log.debug('Reading URL %s', url)
+        log.debug('Received request for title for URL %s', url)
+        for attempt in range(config.MAX_REQUEST_ATTEMPTS):
+            try:
+                start_time = time.monotonic()
+                response = requests.get(url, stream=True, timeout=config.REQUEST_TIMEOUT,
+                                        headers={'User-Agent': config.USER_AGENT})
+                time_used = time.monotonic() - start_time
+                log.debug('Started receiving streaming response in %.2fs.', time_used)
+                response.raise_for_status()
+            except (requests.HTTPError, requests.ConnectionError, requests.ConnectTimeout) as exception:
+                pass
+            break
         return url
-

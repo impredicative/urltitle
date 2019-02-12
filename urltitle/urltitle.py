085fec8f5ae61bb410041b3147f1b890edf173dc
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-11 20:43:09 -0500

    WIP

--- urltitle/urltitle.py
+++ urltitle/urltitle.py
@@ -6,6 +6,7 @@ import requests
 from cachetools.func import ttl_cache
 
 from . import config
+from .util.humanize import humanize_bytes
 
 log = logging.getLogger(__name__)
 
@@ -17,16 +18,30 @@ class CachedURLTitle:
         self.title = ttl_cache(maxsize=cache_max_size, ttl=cache_ttl)(self.title)   # type: ignore  # Instance level cache
 
     def title(self, url: str):
-        log.debug('Received request for title for URL %s', url)
-        for attempt in range(config.MAX_REQUEST_ATTEMPTS):
+        # Can raise: requests.HTTPError, requests.ConnectionError, requests.ConnectTimeout
+        max_attempts = config.MAX_REQUEST_ATTEMPTS
+        request_desc = f'request for title of URL {url}'
+        log.debug('Received %s', request_desc)
+        for num_attempt in range(1, max_attempts + 1):
             try:
                 start_time = time.monotonic()
-                response = requests.get(url, stream=True, timeout=config.REQUEST_TIMEOUT,
-                                        headers={'User-Agent': config.USER_AGENT})
-                time_used = time.monotonic() - start_time
-                log.debug('Started receiving streaming response in %.2fs.', time_used)
-                response.raise_for_status()
+                with requests.get(url, stream=True, timeout=config.REQUEST_TIMEOUT,
+                                  headers={'User-Agent': config.USER_AGENT}) as request:
+                    time_used = time.monotonic() - start_time
+                    content_type = request.headers.get('Content-Type')
+                    content_len = humanize_bytes(request.headers.get('Content-Length'))
+                    log.debug('Started receiving streaming response with declared content type "%s" and content '
+                              'length %s in %.2fs.', content_type, content_len, time_used)
+                    request.raise_for_status()
             except (requests.HTTPError, requests.ConnectionError, requests.ConnectTimeout) as exception:
-                pass
+                exception_desc = f'The exception is: {exception.__class__.__qualname__}: {exception}'
+                log.warning('Error in attempt %s getting %s. %s', num_attempt, request_desc, exception_desc)
+                if isinstance(exception, requests.HTTPError) and (request.status_code == 400):
+                    log.error('Unrecoverable error 400 getting %s. The request will not be reattempted. %s',
+                              request_desc, exception_desc)
+                    raise
+                if num_attempt == max_attempts:
+                    log.error('Exhausted all %s attempts for %s', max_attempts, request_desc)
+                    raise
             break
         return url

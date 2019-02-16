0b7a5d8027f0d1a6c32fe4e18141375d60f7cc60
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-15 22:54:41 -0500

    Remove thread info from logs

--- urltitle/config.py
+++ urltitle/config.py
@@ -26,7 +26,7 @@ LOGGING = {  # Ref: https://docs.python.org/3/howto/logging.html#configuring-log
     'version': 1,
     'formatters': {
         'detailed': {
-            'format': '%(asctime)s %(thread)x-%(threadName)s:%(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
+            'format': '%(asctime)s %(name)s:%(lineno)d:%(funcName)s:%(levelname)s: %(message)s',
         },
     },
     'handlers': {

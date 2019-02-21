57628957dc5a61697d7673e14057dee2004d185b
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-02-20 23:43:46 -0500

    Fallback to XMP if docinfo title is missing in PDF

--- urltitle/util/pikepdf.py
+++ urltitle/util/pikepdf.py
@@ -9,7 +9,13 @@ from io import BytesIO
 
 def _get_pdf_title(pdf_bytes: bytes) -> str:
     import pikepdf  # This must be imported only here, or else it won't work.
-    return str(pikepdf.open(BytesIO(pdf_bytes)).docinfo.get('/Title', '')).strip()
+    pdf = pikepdf.open(BytesIO(pdf_bytes))
+
+    title = str(pdf.docinfo.get('/Title', '')).strip()
+    if not title:
+        with pdf.open_metadata() as metadata:
+            title = str(metadata.get('dc:title', '')).strip()
+    return title
 
 
 def get_pdf_title(pdf_bytes: bytes) -> str:

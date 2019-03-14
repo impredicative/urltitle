9c82198d1d106f8a7afb6717661c50ed5d009edf
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-03-14 18:43:37 -0400

    Bump version to 0.2.1

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     name='urltitle',
     author='Ouroboros Chrysopoeia',
     author_email='impredicative@users.nomail.github.com',
-    version='0.2.0',
+    version='0.2.1',
     description='Get page title or header-based description for URL',
     keywords='url title',
     long_description=Path(__file__).with_name('README.md').read_text().strip(),

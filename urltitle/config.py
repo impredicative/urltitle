60692ac9893cb13cb6964467a2c9f61b5a3a596c
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-07-09 17:22:53 -0400

    Add root logger to properly log bs4 messages

--- urltitle/config.py
+++ urltitle/config.py
@@ -44,6 +44,11 @@ LOGGING = {  # Ref: https://docs.python.org/3/howto/logging.html#configuring-log
         },
     },
     'loggers': {
+        '': {
+            'level': 'WARNING',
+            'handlers': ['console'],
+            'propagate': False,
+        },
         PACKAGE_NAME: {
             'level': 'DEBUG',
             'handlers': ['console'],

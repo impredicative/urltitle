0627e784d8b96b82c191ea5d8a33dc6b03da02c1
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2019-12-29 00:18:03 -0500

    Remove obsolete publish.in reference

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -4,4 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"/..
 
 cd ./requirements
-pip install -U -r ./install.txt -r ./dev.in -r ./publish.in
+pip install -U -r ./install.txt -r ./dev.in

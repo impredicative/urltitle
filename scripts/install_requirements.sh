90037b7cef03d5ef5c2a850b5c89be959744cee1
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-03-01 16:56:55 -0500

    Update pip first

--- scripts/install_requirements.sh
+++ scripts/install_requirements.sh
@@ -3,5 +3,6 @@ set -euxo pipefail
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 cd "${DIR}"/..
 
+pip install -U pip
 cd ./requirements
 pip install -U -r ./install.txt -r ./dev.in

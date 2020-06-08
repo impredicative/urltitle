8d44fdcc6da0a396ed38f4f4ac8d5d3d58da3995
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-06-07 21:08:18 -0400

    Revert accidental change to URL_FILTER

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -145,7 +145,7 @@ TEST_CASES_WITH_BAD_SSL = {
 }
 # pylint: enable=line-too-long
 
-URL_FILTER = "seekingalpha"
+URL_FILTER = "https://google.com"
 
 
 # pylint: disable=missing-class-docstring,missing-function-docstring

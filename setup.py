605d1831cf88f3a6741fc8a28254e77d9493daba
Ouroboros Chrysopoeia <impredicative@users.noreply.github.com>
2020-08-15 22:33:38 -0400

    Bump version from 0.2.45 to 0.2.46

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name="urltitle",
     author="Ouroboros Chrysopoeia",
     author_email="impredicative@users.noreply.github.com",
-    version="0.2.45",
+    version="0.2.46",
     description="Get page title or header-based description for URL",
     keywords="url title",
     long_description=(_DIR / "README.md").read_text().strip(),
